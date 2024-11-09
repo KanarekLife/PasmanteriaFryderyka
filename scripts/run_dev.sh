dadc1901dc77da29592b736a1690012f49d81a9f
fildaw <filippo1056@gmail.com>
2024-11-09 14:03:03 +0100

    feat: db backup and restore from docker container

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -8,16 +8,22 @@ COMPOSE_FILE=${2:-docker-compose.dev.yaml}
 
 case $ACTION in
   start)
-    docker compose -f $COMPOSE_FILE up -d
+    docker compose -f $COMPOSE_FILE up -d prestashop-dev
     ;;
   run)
-    docker compose -f $COMPOSE_FILE up
+    docker compose -f $COMPOSE_FILE up prestashop-dev
     ;;
   stop)
-    docker compose -f $COMPOSE_FILE down
+    docker compose -f $COMPOSE_FILE down prestashop-dev
     ;;
   build)
-    docker compose -f $COMPOSE_FILE build
+    docker compose -f $COMPOSE_FILE build prestashop-dev
+    ;;
+  backup)
+    docker compose -f $COMPOSE_FILE run -e OPERATION=backup --rm prestashop-db-backup-restore
+    ;;
+  restore)
+    docker compose -f $COMPOSE_FILE run -e OPERATION=restore --rm prestashop-db-backup-restore
     ;;
   *)
     echo "Invalid action $ACTION - expected start|run|stop|build"
---
+++ src/db_backup/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+if [ "$OPERATION" = "backup" ]; then
+  echo "Creating backup..."
+  mariadb-dump -P 3306 -h prestashop-db -u root -p$MARIADB_ROOT_PASSWORD --all-databases > db_backup/backup.sql
+elif [ "$OPERATION" = "restore" ]; then
+  echo "Restoring backup..."
+  mariadb -P 3306 -h prestashop-db -u root -p$MARIADB_ROOT_PASSWORD < db_backup/backup.sql
+else
+  echo "Invalid operation $OPERATION - expected backup|restore"
+  exit 1
+fi
