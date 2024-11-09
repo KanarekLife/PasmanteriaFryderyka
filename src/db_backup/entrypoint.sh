447e833933ee7eba28da2fe81fafcf15727d82a8
fildaw <filippo1056@gmail.com>
2024-11-09 20:01:29 +0100

    feat: added env variable for mariadb host

--- src/db_backup/entrypoint.sh
+++ src/db_backup/entrypoint.sh
@@ -3,10 +3,10 @@
 set -e
 if [ "$OPERATION" = "backup" ]; then
   echo "Creating backup..."
-  mariadb-dump -P 3306 -h prestashop-db -u root -p$MARIADB_ROOT_PASSWORD --all-databases > db_backup/backup.sql
+  mariadb-dump -P 3306 -h $MARIADB_HOST -u root -p$MARIADB_ROOT_PASSWORD --all-databases > db_backup/backup.sql
 elif [ "$OPERATION" = "restore" ]; then
   echo "Restoring backup..."
-  mariadb -P 3306 -h prestashop-db -u root -p$MARIADB_ROOT_PASSWORD < db_backup/backup.sql
+  mariadb -P 3306 -h $MARIADB_HOST -u root -p$MARIADB_ROOT_PASSWORD < db_backup/backup.sql
 else
   echo "Invalid operation $OPERATION - expected backup|restore"
   exit 1
