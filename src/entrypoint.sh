e66250f2e287052adb2f89854efff8c7182529b8
fildaw <filippo1056@gmail.com>
2024-11-09 21:39:11 +0100

    fix: permissions, empty .env files

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,7 +2,13 @@
 set -e
 
 envsubst < ./templates/prestashop/app/config/parameters.php > ./prestashop/app/config/parameters.php
-chmod 777 ./prestashop/app/config/parameters.php
+if [ "$DEV_MODE" == "true" ]; then
+    chmod -R 777 ./prestashop
+    echo "Running in development mode"
+else
+    chown -R www-data:www-data ./prestashop
+    chmod -R 755 ./prestashop
+fi
 envsubst < ./templates/prestashop/config/defines_custom.inc.php > ./prestashop/config/defines_custom.inc.php
 
 
