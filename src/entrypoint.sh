58e60be52c9e8953ff3f11237eed4e664d326b36
Krzysztof Nasuta <krzysztof@nasuta.dev>
2024-11-11 12:22:08 +0100

    feat: change prestashop gitignore (#14)
    * feat: change prestashop gitignore
    
    * feat: move changing perms to the dockerfile

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,15 +2,12 @@
 set -e
 
 envsubst < ./templates/prestashop/app/config/parameters.php > ./prestashop/app/config/parameters.php
+envsubst < ./templates/prestashop/config/defines_custom.inc.php > ./prestashop/config/defines_custom.inc.php
+
 if [ "$DEV_MODE" == "true" ]; then
     chmod -R 777 ./prestashop
     echo "Running in development mode"
-else
-    chown -R www-data:www-data ./prestashop
-    chmod -R 755 ./prestashop
 fi
-envsubst < ./templates/prestashop/config/defines_custom.inc.php > ./prestashop/config/defines_custom.inc.php
-
 
 caddy start
 exec php-fpm # without exec, php-fpm will not receive signals
