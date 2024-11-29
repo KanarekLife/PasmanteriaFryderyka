6b7dd079308e411a32e24a80f8eb2fa0be6a5153
Krzysztof Nasuta <krzysztof@nasuta.dev>
2024-11-29 10:05:04 +0100

    Task/25 add unattended tests to ci cd (#26)
    * feat: add token to env
    
    * feat: add test to the workflow
    
    * fix: pushing the image
    
    * chore: disable test start conditions

--- src/db_backup/entrypoint.sh
+++ src/db_backup/entrypoint.sh
@@ -10,6 +10,7 @@ if [ "$OPERATION" = "backup" ]; then
   sed -i -e "s/$MAIL_USER/\$MAIL_USER/g" /tmp/backup.sql
   sed -i -e "s/$MAIL_PASSWORD/\$MAIL_PASSWORD/g" /tmp/backup.sql
   sed -i -e "s/$PRESTASHOP_URL/\$PRESTASHOP_URL/g" /tmp/backup.sql
+  sed -i -e "s/$API_KEY/\$API_KEY/g" /tmp/backup.sql
 
   mv /tmp/backup.sql ./backup.sql
 elif [ "$OPERATION" = "restore" ]; then
@@ -21,6 +22,7 @@ elif [ "$OPERATION" = "restore" ]; then
   sed -i -e "s/\$MAIL_USER/$MAIL_USER/g" /tmp/backup.sql
   sed -i -e "s/\$MAIL_PASSWORD/$MAIL_PASSWORD/g" /tmp/backup.sql
   sed -i -e "s/\$PRESTASHOP_URL/$PRESTASHOP_URL/g" /tmp/backup.sql
+  sed -i -e "s/\$API_KEY/$API_KEY/g" /tmp/backup.sql
 
   mariadb -P 3306 -h $MARIADB_HOST -u $MARIADB_USER -p$MARIADB_PASSWORD $MARIADB_DATABASE </tmp/backup.sql
   rm /tmp/backup.sql
